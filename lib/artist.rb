81c45c2b72bc8cd7cf56bc35026b4db44842cf89
Katie Blizzard <kblizzard11@gmail.com>
2019-03-24 20:29:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,8 @@ require 'pry'
 class Artist
   
   extend Memorable
-  extend Findable 
+  extend Findable
+  include Paramable 
   
   attr_accessor :name
   attr_reader :songs
@@ -28,7 +29,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
